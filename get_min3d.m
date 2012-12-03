fa07b2ea2b7a5d9c2d8c76e286fe9a05b0d4d4e1
azardilis <azardilis@gmail.com>
2012-12-03 10:48:48 +0000

    vectorised one of the for loops with big performance increase

--- get_min3d.m
+++ get_min3d.m
@@ -1,14 +1,11 @@
-function [ind, score] = get_min3d(current_pixel, next_pixel, col, row, scores, im, m)
-  score = 50000;
-  ind = 0;
-
-  for i=1:m
-    loc1 = [i, col-1]; #previous pixel
-    eng = energy(loc1, current_pixel, next_pixel, im);
-    sc = scores(i, row) + eng;
-    if sc < score
-      score = sc;
-      ind = i;
-    end
-  end
+function [ind, score] = get_min3d(current_pixel, next_pixel,scores, int, m)
+  lambda = 0.5;
+  row = current_pixel(1);
+  col = current_pixel(2);
+  loc1 = [(1:m)', ones(m, 1)*(col-1)];
+  loc2 = [ones(m, 1)*current_pixel(1), ones(m, 1)*current_pixel(2)];
+  loc3 = [ones(m, 1)*next_pixel(1), ones(m, 1)*next_pixel(2)];
+  engs = lambda * magnitude((loc3 - 2*loc2 + loc1)') ./ magnitude((loc3 - loc1)') + (1 - lambda) * int;
+  scs = scores + engs';
+  [score, ind] = min(scs);
 end
